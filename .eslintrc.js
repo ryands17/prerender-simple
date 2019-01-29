1cc6e2a8602a03b0633268ec55de05f67bd7d6f6
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2019-01-30 00:08:11 +0530

    initial commit with lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: 'standard',
+  rules: {
+    'space-before-function-paren': 0,
+    'no-unsafe-finally': 0,
+  },
+}
---
+++ src/index.js
@@ -0,0 +1,26 @@
+const puppeteer = require('puppeteer')
+
+async function crawler({ url }) {
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
+
+  let page = null
+  let html = false
+
+  try {
+    page = await browser.newPage()
+    // networkidle0: consider navigation to be finished when
+    // there are no more than 2 network connections for at least 500 ms.
+    // (https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagegobackoptions)
+    await page.goto(url, { waitUntil: 'networkidle0' })
+    html = await page.content()
+  } catch (e) {
+    console.warn(`Not able to fetch ${url}`)
+  } finally {
+    if (page) {
+      await page.close()
+    }
+    return html
+  }
+}
+
+module.exports = crawler
